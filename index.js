23f8b8817d0aa74173d0385105ffafb03ef8f48e
John Doe <johndoe@example.com>
2021-03-09 18:31:43 +0900

    ccc

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function Ac() {
  total.innerHTML="0"
 }
  function num() {
+	 let ww = document.querySelector('.digit');
+     console.log(ww.innerText)
 	 let aa = document.getElementsByClassName('digit')
 	 let nums = document.getElementsByClassName('digit')[9]
 	 console.log(nums.innerText);
